c97715bc5110ae5ff546f81853d9fbbae2c190cf
miorembran <67882285+suiseimajo@users.noreply.github.com>
2025-07-25 17:44:50 -0300

    Componente de voltar página

--- config/process.php
+++ config/process.php
@@ -5,14 +5,38 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id;
 
-    $query = "SELECT * FROM contacts";
+    if (!empty($_GET)) {
+        $id = $_GET["id"];
+    }
 
-    $stmt = $conn->prepare($query);
+    // Retorna o dado de um contato
+    if (!empty($id)) {
 
-    $stmt->execute();
+        $query = "SELECT * FROM contacts WHERE id = :id";
 
-    $contacts = $stmt->fetchAll();
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+
+    } else {
+
+        // Retorna todos os contatos
+        $contacts = [];
+
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+        
+    }
 
 ?>
