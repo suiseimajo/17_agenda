a792ad55badfdaff461fdb9016d078efcb235cce
miorembran <67882285+suiseimajo@users.noreply.github.com>
2025-07-30 15:07:41 -0300

    Removendo contatos

--- config/process.php
+++ config/process.php
@@ -36,6 +36,56 @@
                 echo "Erro: $error";
             }
 
+        } else if ($data["type"] === "edit") {
+
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = "UPDATE contacts 
+            SET name = :name, phone = :phone, observations = :observations 
+            WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato atualizado com sucesso!";
+
+            } catch (PDOException $e) {
+                // Erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+
+        } else if ($data["type"] === "delete") {
+
+            $id = $data["id"];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":id", $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso!";
+
+            } catch (PDOException $e) {
+                // Erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+
         }
 
         // Redirect HOME
